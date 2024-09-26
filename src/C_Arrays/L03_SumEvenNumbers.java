c360b1fab5366344b958bfb6d08aa5f36c4907a1
stoynko <yani.stoyanov@icloud.com>
2024-09-26 16:32:15 +0300

    [26/09/2024] Update

--- src/C_Arrays/L03_SumEvenNumbers.java
+++ src/C_Arrays/L03_SumEvenNumbers.java
@@ -1,9 +1,24 @@
 package C_Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class L03_SumEvenNumbers {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int[] numbers = Arrays.stream(scanner.nextLine()
+                        .split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
 
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                sum += number;
+            }
+        }
+        System.out.println(sum);
     }
 }
